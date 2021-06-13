473e34261216df063bf02003697e28a817b8b9d4
moritasoshi <soshi.morita@rakus-partners.co.jp>
2021-06-13 19:34:49 +0900

    stop event bubbling

--- src/components/BasicDemo.tsx
+++ src/components/BasicDemo.tsx
@@ -4,12 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Tabs, DefaultTabBar } from "rmc-tabs";
 
-import css from "./BasicDemo.module.scss"
+import css from "./BasicDemo.module.scss";
 
 const tabData = [
-  { title: "title 1" },
-  { title: "title 2" },
-  { title: "title 3" }
+  { key: "t1", title: "title 1" },
+  { key: "t2", title: "title 2" },
+  { key: "t3", title: "title 3" },
 ];
 
 class BasicDemo extends React.Component<{}, any> {
@@ -17,7 +17,7 @@ class BasicDemo extends React.Component<{}, any> {
     super(props);
     this.state = {
       scData: JSON.stringify({ index: 0, tab: { title: "t1" } }),
-      scData2: JSON.stringify({ index: 0, tab: { title: "t1" } }),
+      scData2: JSON.stringify({ index: 1, tab: { title: "t2" } }),
       dynamicTabs: [] as { title: string }[],
     };
   }
@@ -36,12 +36,17 @@ class BasicDemo extends React.Component<{}, any> {
         <div style={{ ...baseStyle }}>
           <Tabs
             tabs={tabData}
+            useOnPan={false}
             onChange={(tab, index) => {
               this.setState({
                 scData: JSON.stringify({ index: index + Math.random(), tab }),
               });
             }}
-            renderTabBar={(props) => <DefaultTabBar {...props} />}
+            renderTabBar={(props) => <DefaultTabBar {...props} page={3} />}
+            destroyInactiveTab={true}
+            tabBarActiveTextColor={"#917100"}
+            tabBarUnderlineStyle={{ border: "1px #dbab00 solid" }}
+            tabBarTextStyle={{ color: "#666666" }}
           >
             <div style={{ padding: 10, background: "#ADFFD7", height: 1000 }}>
               <p>single content</p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,64 @@
-import * as React from 'react';
-import css from './index.module.scss'
+import * as React from "react";
+import css from "./index.module.scss";
 
-import BasicDemo from "../components/BasicDemo"
+import BasicDemo from "../components/BasicDemo";
+import { parseBody } from "next/dist/next-server/server/api-utils";
+
 const Home: React.FC = () => {
+  React.useEffect(() => {
+    let touchStartX: number;
+    let touchStartY: number;
+
+    let touchEndX: number;
+    let touchEndY: number;
+
+    const getStartCoordinate = (e: TouchEvent) => {
+      const touchObj = e.changedTouches[0];
+      touchStartX = touchObj.pageX;
+      touchStartY = touchObj.pageY;
+    };
+
+    const getEndCoordinate = (e: TouchEvent) => {
+      const touchObj = e.changedTouches[0];
+      touchEndX = touchObj.pageX;
+      touchEndY = touchObj.pageY;
+    };
+
+    // 角度を計算
+    const calcDegree = (x1, y1, x2, y2) => {
+      const radian = Math.atan2(y2 - y1, x2 - x1);
+      return radian * (180 / Math.PI);
+    };
+
+    // 上下左右の方向が曖昧なスクロールを禁止
+    const limitSwipe = (e: TouchEvent) => {
+      getEndCoordinate(e);
+      const degree = calcDegree(touchStartX, touchStartY, touchEndX, touchEndY);
+      const degreeAbs = Math.abs(degree);
+      if (degreeAbs >= 30 && degreeAbs <= 60) {
+        e.stopImmediatePropagation();
+      } else if (degreeAbs >= 120 && degreeAbs <= 150) {
+        e.stopImmediatePropagation();
+      }
+      console.log(`degree: ${degree}`);
+    };
+
+    if (process.browser) {
+      const body = document.querySelector("body");
+      const next = document.getElementById("index_container");
+
+      next.addEventListener("touchstart", getStartCoordinate, {
+        passive: false,
+      });
+      next.addEventListener("touchend", limitSwipe);
+      return () => {};
+    }
+  });
   return (
-    <div className={css.container}>
+    <div className={css.container} id={"index_container"}>
       <BasicDemo></BasicDemo>
     </div>
   );
-}
+};
 
 export default Home;
